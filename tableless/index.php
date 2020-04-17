64016dbdebf3768f73ba1b2fc109a2100e167ce0
Douglas Morais <douglaserik.tec@gmail.com>
2020-04-16 20:21:31 -0400

    Criação do esqueleto da página header / footer / content

---
+++ tableless/index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>UPNEWS</title>
+    <!--CSS Manual-->
+    <link href="style.css" rel="stylesheet" type="text/css">
+    
+</head>
+<body>
+    <div id="box">
+
+        <div id="header">
+            <div id="header_logo">
+               Logomarca
+            </div><!--Fechamento da div header_logo-->
+
+            <div id="header_anuncio">
+                Anuncio Header
+            </div><!--Fechamento da div header_anuncio-->
+
+            <div id="header_surch">
+                surch
+            </div><!--Fechamento da div header_surch-->
+
+            <div id="header_menu">
+                Menu
+            </div><!--Fechamento da div header_menu-->
+
+        </div><!--Fechamento da div header-->
+
+        <div id="sidebar_left">
+            LEFT SIDEBAR
+        </div><!--Fecha a left sidebar-->
+
+        <div id="content">
+            <div id="content_destaque_conteudo">
+                Lista do conteudo
+            </div><!--content destaque conteudo-->
+
+            <div id="content_destaque_destaque">
+                Destaque
+            </div><!--content_destaque_destaque -->
+
+            <div id="content_entreterimento">
+            Entreterimento
+            <div id="content_entreterimento_conteudo">
+                Entreterimento conteudo
+            </div><!-- content_entreterimento_conteudo -->
+
+            <div id="content_entreterimento_anuncio">
+                Entreterimento anuncios
+            </div><!--  content_entreterimento_anuncio -->
+
+            </div><!-- content_entreterimento -->
+
+            <div id="contet_esportes">
+            <div id="content_esportes_conteudo">
+                Esportes conteudo
+            </div><!--  content_esportes_conteudo -->
+
+            <div id="content_esportes_comentados">
+                content esportes comentados
+            </div><!-- content_esportes_comentados -->
+        </div><!-- content entreterimento -->
+
+        <div id="contet_tecnologia">
+
+            <div id="content_tecnologia_destaque">
+                Tecnologia Destaques
+            </div>
+
+            <div id="content_tecnologia_conteudo">
+                Tecnologia Conteudo
+            </div>
+
+        </div><!-- contet_tecnologia -->
+
+        <div id="content_vistos">
+
+            <div id="content_vistos_conteudo">
+                Content vistos conteudo
+            </div>
+
+            <div id="content_vistos_anuncio">
+                Content vistos anuncio
+            </div>
+
+        </div><!-- Content Vistos -->
+
+        </div><!--content -->
+
+        
+
+
+
+        <div id="right_sidebar">
+            sidebar right
+        </div>
+        
+    <div id="clear"></div>
+
+    </div><!--Fechamento da div box-->
+    
+    <div id="footer_all">    
+        
+        <div id="footer">
+            <div id="footer_txt">
+                footer txt
+            </div><!--Fechamento da div footer_txt-->
+        </div><!--Fechamento da div footer-->
+
+    </div><!--Fechamento da div footer_all-->
+</body>
+</html>
