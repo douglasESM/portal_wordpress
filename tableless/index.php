663a005c2e465bb7174236dd29a004f65e8e8537
Douglas Erik da Silva Morais <douglasmorais@HJC.local>
2020-05-02 11:36:52 -0400

    Aula 17 - Formatando Sidebar Left

--- tableless/index.php
+++ tableless/index.php
@@ -32,13 +32,60 @@
             </div><!--Fechamento da div header_surch-->
 
             <div id="header_menu">
-                Menu
+                
             </div><!--Fechamento da div header_menu-->
 
         </div><!--Fechamento da div header-->
 
         <div id="sidebar_left">
-            LEFT SIDEBAR
+            <h1 class="noticias">/noticias</h1>
+            <ul>
+                <li><a href="#">sub categoria 01</a></li>
+                <li><a href="#">sub categoria 02</a></li>
+                <li><a href="#">sub categoria 03</a></li>
+                <li><a href="#">sub categoria 04</a></li>
+                <li><a href="#">sub categoria 05</a></li>
+            </ul>
+
+            <h1 class="esportes">/esportes</h1>
+            <ul>
+                <li><a href="#">sub categoria 01</a></li>
+                <li><a href="#">sub categoria 02</a></li>
+                <li><a href="#">sub categoria 03</a></li>
+                <li><a href="#">sub categoria 04</a></li>
+                <li><a href="#">sub categoria 05</a></li>
+            </ul>
+
+            <h1 class="entreterimento">/entreterimento</h1>
+            <ul>
+                <li><a href="#">sub categoria 01</a></li>
+                <li><a href="#">sub categoria 02</a></li>
+                <li><a href="#">sub categoria 03</a></li>
+                <li><a href="#">sub categoria 04</a></li>
+                <li><a href="#">sub categoria 05</a></li>
+            </ul>
+
+            <h1 class="tecnologia">/tecnologia</h1>
+            <ul>
+                <li><a href="#">sub categoria 01</a></li>
+                <li><a href="#">sub categoria 02</a></li>
+                <li><a href="#">sub categoria 03</a></li>
+                <li><a href="#">sub categoria 04</a></li>
+                <li><a href="#">sub categoria 05</a></li>
+            </ul>
+
+            <h1 class="especial">/especial</h1>
+            <ul>
+                <li><a href="#">sub categoria 01</a></li>
+                <li><a href="#">sub categoria 02</a></li>
+                <li><a href="#">sub categoria 03</a></li>
+                <li><a href="#">sub categoria 04</a></li>
+                <li><a href="#">sub categoria 05</a></li>
+            </ul>
+
+            <div id="left_adsence">
+                <img src="imagens/anuncio2.png"/>
+            </div>
         </div><!--Fecha a left sidebar-->
 
         <div id="content">
