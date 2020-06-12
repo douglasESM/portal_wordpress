17d34dfff9db6a82cd75bb32e1d7c3b2e3adf4ee
Douglas Morais <douglaserik.tec@gmail.com>
2020-06-12 10:16:02 -0400

    Atualizando scripts dos arquivos do remoto desktop

--- site/wp-content/themes/upnews/page.php
+++ site/wp-content/themes/upnews/page.php
@@ -6,13 +6,15 @@
 
     <div id="content">
         <div class="page">
-                <?php if (have_posts ()): while (have_posts()): the_post(); ?>
 
-                    <span id="titulo"><?php the_title();  ?></span>
-                    <?php the_content(); ?>
+            <?php if (have_posts ()): while (have_posts()): the_post(); ?>
 
-                <?php endwhile; else: ?>
-                <?php endif; ?>
+                <span id="titulo"><?php the_title();  ?></span>
+                <?php the_content(); ?>
+
+            <?php endwhile; else: ?>
+            <?php endif; ?>
+            
         </div>
     </div><!--content -->
 
