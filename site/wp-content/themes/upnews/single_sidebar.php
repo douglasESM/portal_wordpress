bee1834b9f2dc168b3cce04416bbe794da5d4f12
Douglas Morais <douglaserik.tec@gmail.com>
2020-07-01 11:30:56 -0400

    Aula 53 - Sidebar com dados do autor

--- site/wp-content/themes/upnews/single_sidebar.php
+++ site/wp-content/themes/upnews/single_sidebar.php
@@ -1,5 +1,19 @@
 <div id="dados_autor">
     <h1>Dados do Autor</h1>
+
+    <!-- avatar -->
+    <span class="avatar"><?php echo get_avatar( get_the_author(), 80 ); ?></span>
+    <!-- nome -->
+    <span class="nome"><?php the_author_firstname();?> <?php the_author_lastname(); ?></span>
+    <!-- descrição -->
+    <p><?php the_author_description(); ?></p>
+    <!-- email -->  
+    <a href="<?php the_author_email();?>" title="<?php the_author_email();?>">Email</a>
+    <!-- site -->
+    <a href="<?php the_author_url();?>" title="<?php the_author_url();?>">Site</a>
+    <!-- author -->
+    <a href="<?php bloginfo('url');?>/author=<?php the_author_id();?>">/Blog</a>
+
 </div><!-- fim dados_autor -->
 
 <div id="dados_posts">
@@ -10,6 +24,6 @@
     <h1>Publicidade</h1>
 </div><!-- fim anuncios do google -->
 
-<div id="relacionados">
-    <h1>Tópicos Relacionados</h1>
+<div id="noticias">
+    <h1>Noticias</h1>
 </div><!--fim relacionados -->
